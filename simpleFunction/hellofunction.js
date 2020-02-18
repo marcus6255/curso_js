b9bd945d0f98f01ba5703bace7e3e33bb2e26120
Marcelo Marcus Nogueira Martins <marcelo@cglam.local>
2020-02-18 13:27:34 -0400

    curso de JS

---
+++ simpleFunction/hellofunction.js
@@ -0,0 +1,9 @@
+/**
+ * aula 4
+ */
+
+ function hello() {
+     var nome;
+     nome = prompt("Qual é o seu nome?");
+     alert("Hello"+ nome);
+ }
---
+++ vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * JS - Aula 3: Vinculando um documento ao JS
+ * @author Professor Marcelo Martins
+ */
+
+ document.writ("<h2>"+ "Professor Marcelo Martins" + "</h2>");
